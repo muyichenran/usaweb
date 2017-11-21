66d62227c8554e9da6d1326af5baf95f58745cc2
muyichenran <961774108@qq.com>
2017-11-21 19:07:51 +0800

    修改

--- src/components/merchandise/addAttribute.vue
+++ src/components/merchandise/addAttribute.vue
@@ -2,33 +2,46 @@
   	<!--添加属性管理-->
   	<div class="admin-center">
 		<div class="attribute-block">
-			
-		</div>	
-			<div v-if="addSize3Show"> 
-					<div class="box-shade"></div>
-					<div class="add-color-box">
-							<a @click="addSize3Show=false" class="close" href="javascript:;">
-						<i class="iconfont">&#xe605;</i>
-					</a>
-							<table>
-								<tr>
-									<td width="120" align="right">Size Name</td>
-									<td width="220">
-										<el-input v-model="shoesSizeObj.Name"></el-input>
-									</td>
-								</tr>
-								<tr>
-									<td width="120" align="right">Size Number</td>
-									<td width="220">
-										<el-input v-model="shoesSizeObj.Number"></el-input>
-									</td>
-								</tr>
-							</table>
-							<p class="align-center">
-									<el-button @click="subSize3Fun()" type="primary"  class="submit-btn">Submit</el-button>
-							</p>
-					</div>
+			<div class="attribute-block-top">
+				<el-button type="primary" plain>选择分区</el-button>
+				<span class="attribute-name">衬衣分区</span>
 			</div>
+		</div>	
+
+
+		<div class="select-division">
+			<div class="box-shade"></div>
+			<div class="usa-up-box">
+				<div class="division-list"></div>
+			</div>
+		</div>
+
+
+		<div v-if="addSize3Show"> 
+				<div class="box-shade"></div>
+				<div class="add-color-box">
+						<a @click="addSize3Show=false" class="close" href="javascript:;">
+					<i class="iconfont">&#xe605;</i>
+				</a>
+						<table>
+							<tr>
+								<td width="120" align="right">Size Name</td>
+								<td width="220">
+									<el-input v-model="shoesSizeObj.Name"></el-input>
+								</td>
+							</tr>
+							<tr>
+								<td width="120" align="right">Size Number</td>
+								<td width="220">
+									<el-input v-model="shoesSizeObj.Number"></el-input>
+								</td>
+							</tr>
+						</table>
+						<p class="align-center">
+								<el-button @click="subSize3Fun()" type="primary"  class="submit-btn">Submit</el-button>
+						</p>
+				</div>
+		</div>
   	</div>
 </template>
 
@@ -36,7 +49,7 @@
 export default {
    data(){
       return{
-      	addSize3Show:false,
+		    addSize3Show:false,
       }
     },
     components: {
@@ -50,6 +63,23 @@ export default {
 }
 </script>
 
-<style lang="scss">
-	
+<style lang="scss" scoped>
+	.attribute-block{
+		.attribute-block-top{
+			.attribute-name{
+				color: #409eff;
+				padding-left: 30px;
+				font-size: 16px;
+			}
+		}
+	}
+	.select-division{
+		.usa-up-box{
+			width: 400px;
+			padding: 20px;
+			height: 400px;
+			overflow: hidden;
+		}
+	}
 </style>
+
--- src/components/merchandise/subdivision.vue
+++ src/components/merchandise/subdivision.vue
@@ -190,16 +190,16 @@ export default {
 		}
 		.add-sub-box{
 			z-index: 100;
-	    padding: 30px;
-	    position: fixed;
-	    width: 400px;
-	    left: 50%;
-	    top: 50%;
-	    -webkit-transform: translate(-50%,-50%);
-	    transform: translate(-50%,-50%);
-	    border-radius: 10px;
-	    background: #fff;
-	    box-shadow: 0 2px 3px rgba(0,0,0,.4);
+			padding: 30px;
+			position: fixed;
+			width: 400px;
+			left: 50%;
+			top: 50%;
+			-webkit-transform: translate(-50%,-50%);
+			transform: translate(-50%,-50%);
+			border-radius: 10px;
+			background: #fff;
+			box-shadow: 0 2px 3px rgba(0,0,0,.4);
 	    .close{
 	    	position: absolute;
 	    	top: 5px;
